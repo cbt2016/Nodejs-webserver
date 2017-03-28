01c4b1ba895272114e966d4102bc006980c43103
cbt2016 <simotouil88@gmail.com>
2017-03-28 10:16:00 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var hbs = require('hbs');
+var fs = require('fs');
+var app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+
+app.use((req,res,next)=>{
+   var now = new Date().toString();    
+   var log = `${now} : ${req.method} : ${req.url} \n`;
+   console.log(log);   
+   fs.appendFile('server-log.txt',log,(err)=>{
+       if(err){
+           console.log(err.message);
+       }
+   });
+   next();    
+});
+
+app.use((req,res,next)=>{
+   res.render('maintenance.hbs');
+       
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle: 'About us:'
+    });
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage: '404 Not Found'
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('server running on port 3000');
+});
